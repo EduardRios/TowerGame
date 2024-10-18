8db93dda3e8420654f470db26358cd621cd2ad00
EdRios1608 <89537754+EdRios1608@users.noreply.github.com>
2024-10-18 12:29:27 -0500

    First commit

---
+++ src/app/Index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Greet from './components/Greet';
+
+
+const Index = () => {
+  return (
+    <View>
+        <Greet/>
+
+    </View>
+  )
+};
+
+
+export default Index;
+
+
---
+++ src/app/components/Block.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+const Greet = () => {
+    return <Text>Hello, what up</Text>
+};
+
+export default Greet;
+
