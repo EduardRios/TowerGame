499dc5cddef36f2334827108f4d09cf2acaf20e1
EdRios1608 <89537754+EdRios1608@users.noreply.github.com>
2024-10-21 11:31:23 -0500

    Found some bugs and solved them

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import Block from './Block'; 
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 
 const Index = () => {
 
+    
+
     const [block, setBlock] = useState(new Block(100, 20, 50, 100, 5)); 
 
+    //Base Block
+    const baseBlockWidth = 150;
+    const baseBlockHeight = 20;
+    const baseBlock = new Block(
+        baseBlockWidth, 
+        baseBlockHeight, 
+        screenWidth/2, 
+
+        screenHeight-100, //problem here
+        0
+    );
+
+    //Effect to move edge to edge
     useEffect(() => {
         const interval = setInterval(() => {
             block.moveInX(); 
-
             setBlock(new Block(block.width, block.height, block.xPosition, block.yPosition, block.Xspeed));
         }, 16);
-
-        
         return () => clearInterval(interval); 
     }, []); 
 
+
+    //View Components
     return (
         <View style={styles.containerStyle}>
+
             <View
                 style={[
                     styles.blockStyle,
@@ -30,8 +48,25 @@ const Index = () => {
                     }
                 ]}
             />
+            
+
+            <View
+                style={[
+                    styles.baseBlockStyle,
+                    {
+                        width: baseBlock.width,
+                        height: baseBlock.height,
+                        top: baseBlock.yPosition
+                        
+                    },
+                ]}
+            />
+
+                
+
         </View>
     );
+
 };
 
 const styles = StyleSheet.create({
@@ -44,6 +79,13 @@ const styles = StyleSheet.create({
     blockStyle: {
         position: 'absolute',
         backgroundColor: 'blue',  
+    },
+
+    baseBlockStyle: {
+        position: 'absolute',
+        backgroundColor: 'green', 
+        borderTopWidth: 2,
+        borderColor: 'black',    
     },
 });
 
